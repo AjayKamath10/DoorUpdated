04120844bf5e0ea70073484e20e019b8bdf97d2c
AjayKamath10 <84276017+AjayKamath10@users.noreply.github.com>
2024-01-24 23:27:04 +0530

    Changes

---
+++ DoorFunctionsLibrary/AddOn.cs
@@ -0,0 +1,16 @@
+﻿using DoorFunctionsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorFunctionsLibrary
+{
+    public abstract class AddOn
+    {
+        public abstract void Alert(SmartDoor door);
+
+        
+    }
+}
---
+++ DoorFunctionsLibrary/Timer.cs
@@ -0,0 +1,34 @@
+﻿using DoorFunctionsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorFunctionsLibrary
+{
+    public class Timer : ITimerControl
+    {
+        private SmartDoor door;
+        public Timer(SmartDoor Door)
+        {
+            this.door = Door; 
+        }
+
+        public event Action<SmartDoor> TimerThresholdReachedEvent;
+        public void StartTimer(int Threshold)
+        {
+            Thread timerThread = new Thread(() => { 
+            Thread.Sleep(Threshold * 1000);
+
+            if (door.state == DoorStates.OPEN) door.ThresholdReached();
+
+            });
+            timerThread.Start();
+
+        }
+       
+
+
+    }
+}
