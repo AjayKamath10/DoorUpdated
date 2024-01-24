4604fbfa43be4fe003138a7741fd770b26114ee4
Ajay Kamath <ajayvkamath10@gmail.com>
2024-01-24 23:17:23 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using DoorFunctionsLibrary;
+
+namespace Door
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Buzzer Buzzer1 = new Buzzer();
+            Pager Pager1 = new Pager();
+            AutoClose AutoClose1 = new AutoClose();
+
+            SimpleDoor Door1 = new SimpleDoor("Door1");
+            SmartDoor Door2 = new SmartDoor("Door2");
+            Door2.SetTimer(2);
+
+            Buzzer1.Subscribe(Door2);
+            Pager1.Subscribe(Door2);
+            AutoClose1.Subscribe(Door2);
+
+
+
+            Door2.Open();
+
+            Console.ReadKey();
+
+         
+        }
+    }
+}
